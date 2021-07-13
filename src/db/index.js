686162fcea2f9bff158e6f7ae92b91013851b24b
khushab <khushabalam7@gmail.com>
2021-07-13 21:05:09 +0530

    users route setup

--- src/db/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 3200
-
-app.listen(port, () => {
-    console.log('server is running on ' + port)
-})
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,56 +6,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is Invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password should contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
 
-const me = new User({
-    name: "    Khushab  ",
-    email: "KHUshabalam7@GMAIl.com      ",
-    age: 22
-})
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('error', error)
-})
+// const me = new User({
+//     name: "    Khushab  ",
+//     email: "KHUshabalam7@GMAIl.com      ",
+//     age: 22
+// })
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('error', error)
+// })
 
 const Task = mongoose.model('Task', {
     description: {

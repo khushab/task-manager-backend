f30751eda5622974f864ce0d8faed83730527fa1
khushab <khushabalam7@gmail.com>
2021-07-16 21:43:53 +0530

    users and tasks get APIs added

--- src/index.js
+++ src/index.js
@@ -18,6 +18,26 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(404).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -25,6 +45,26 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((err) => {
         res.status(400).send(err)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(400).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send()
     })
 })
 

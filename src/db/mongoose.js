8bac42ea4e8c9c381a9b5be3310ac2c70357d3ab
khushab <khushabalam7@gmail.com>
2021-07-18 19:02:59 +0530

    deleted comments in mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,30 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-
-
-// const me = new User({
-//     name: "    Khushab  ",
-//     email: "KHUshabalam7@GMAIl.com      ",
-//     age: 22
-// })
-
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('error', error)
-// })
-
-
-
-// const newTask = new Task({
-//     description: "First Task",
-//     completed: false,
-// })
-
-// newTask.save().then(() => {
-//     console.log(newTask);
-// }).catch((error) => {
-//     console.log('error', error)
-// })

0bddb51fe2f01728b03778ebbac24bff002211c7
khushab <khushabalam7@gmail.com>
2021-08-07 15:31:08 +0530

    json web token added in signup and login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,29 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisisasecretkey')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+
+}
+
+//finding user by email and password
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email: email })
 

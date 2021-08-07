faed1e354f69db89081413a55d9525b06d07d7eb
khushab <khushabalam7@gmail.com>
2021-08-07 17:05:10 +0530

    authentication, profile route added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+
+//middleware for maintanance
+// app.use((req, res) => {
+//     res.status(503).send("Server in maintainance. We'll be back soon!")
+// })
+
 const app = express();
 const port = process.env.PORT || 3200
 

b708e82195beb6d9fd28964d6411f52063c77f25
khushab <khushabalam7@gmail.com>
2021-08-30 19:45:04 +0530

    modified task model

--- src/models/task.js
+++ src/models/task.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose')
 
+//enums
+const priorityType = ['high', 'medium', 'low'];
+
 const Task = mongoose.model('Task', {
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     description: {
         type: String,
         required: true,
         trim: true,
     },
+    priority: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!priorityType.includes(value)) {
+                throw new Error('Invalid Priority')
+            }
+        }
+    },
     completed: {
         type: Boolean,
         default: false,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     }
 })
 

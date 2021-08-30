bf1b9ef566864c2ea74599e943e228752a32894b
khushab <khushabalam7@gmail.com>
2021-08-30 13:03:18 +0530

    hiding private data when returning a user

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,19 @@ const userSchema = new mongoose.Schema({
     ]
 })
 
+
+//removing not required data when sending the user back
+userSchema.methods.toJSON = function () {
+    const user = this
+
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisisasecretkey')
